e76179e3eba12bc806df81671b57f1ce3df4c677
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-13 15:50:41 +0200

    first part for the backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,12 @@
+import User from "../models/User.model.js";
+
 export const getUserForSidebar = async (req, res) => {
     try{
         const loggedInUserId = req.user._id;
         console.log(loggedInUserId);
+
+        const allUser = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+        res.status(200).json(allUser);
     }catch (error){
         console.log(error);
         res.status(500).json({message: "Internal server error"});
