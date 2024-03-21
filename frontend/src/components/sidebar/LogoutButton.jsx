1a4387d5b4bf22abd0957f0ebf27451e92529a41
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-21 23:07:51 +0200

    complete the project

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,19 +1,17 @@
-import { TbLogout2 } from "react-icons/tb";
-import  useLogout from "../../hooks/useLogout";
-const LogoutButton = () => {
-  const {loading,logout}= useLogout();
-  return (
-    <div className="mt-auto">
-      {!loading ? (
-        <TbLogout2 className="w-6 h-6 text-white cursor-pointer"
-        onClick={logout}
-        />
-      ) : (
-        <span className="loading loading-spinner"></span>
-      )}
-        
-    </div>
-  )
-}
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
-export default LogoutButton
+const LogoutButton = () => {
+	const { loading, logout } = useLogout();
+
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
+export default LogoutButton;
